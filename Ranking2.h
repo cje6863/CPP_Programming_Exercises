5ca1b72843178ca238705b88b5966b40f5ca7df3
cje6863 <103155872+cje6863@users.noreply.github.com>
2022-04-08 14:20:31 +0900

    Add files via upload
    week4_with_result

---
+++ Ranking2.h
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <stdio.h>
+
+struct PlayInfo
+{
+	char name[200];
+	int nMove;
+	double tEapsed;
+};
+extern void loadRanking(const char* filename);
+extern void storeRanking(const char* filename);
+extern void printRanking();
+extern int addRanking(int nMove, double tElap);
